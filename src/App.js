2470dd07b99e71fa19c017a2607643b6d217f7ce
Andrew Aluko <aluko17@icloud.com>
2019-03-27 16:03:50 -0500

    results

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Results from './Components/Results'
 import './App.css';
 
 class App extends Component {
@@ -7,6 +7,7 @@ class App extends Component {
     return (
       <div>
       <h1>Calculator</h1>
+      <Results />
 
       </div>
     );
---
+++ src/Components/Results.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class Results extends Component {
+
+	render(){
+		return(
+		<div>
+		<h1>Results</h1>
+		</div>
+
+		)
+	}
+
+}
